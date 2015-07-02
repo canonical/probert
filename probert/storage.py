cac1996a70eff05c43a753be3fb579b05bd2b3ff
Ryan Harper <ryan.harper@canonical.com>
2015-07-02 14:36:17 -0500

    Add Storage and Network Info classes
    Implement *Info classes which provide a simple
    interface of properties on objects built from
    probe results.
    
    StorageInfo can be built from any of the top
    level keys from results['storage'].keys()
    
    The properties attempt to find the the attribute in the
    results dictionary by fallback to other possible values
    to find an value.
    
    Fix python coverage report by including .coveragerc file
    
    Add testing data and improve and extend unit testing.
    
    Add logging support.
    
    Signed-off-by: Ryan Harper <ryan.harper@canonical.com>

--- probert/storage.py
+++ probert/storage.py
@@ -14,15 +14,128 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import pyudev
-from probert.utils import udev_get_attribute
 import os
+import logging
+
+from probert.utils import udev_get_attribute
+
+log = logging.getLogger('probert.storage')
+
+
+class StorageInfo():
+    ''' properties:
+        .type = [disk, partition, etc.}
+        .name = /dev/sda
+        .size = 123012034 (bytes)
+        .serial = abcdefghijkl
+        .vendor = Innotec
+        .model = SolidStateRocketDrive
+        .devpath = /devices
+        .is_virtual =
+        .raw = {raw dictionary}
+    '''
+    def __init__(self, probe_data):
+        [self.name] = probe_data
+        self.raw = probe_data.get(self.name)
+
+        self.type = self.raw['DEVTYPE']
+
+    def _get_hwvalues(self, keys, missing='Unknown value'):
+        for key in keys:
+            try:
+                return self.raw[key]
+            except KeyError:
+                log.debug('Failed to get key '
+                          '{} from interface {}'.format(key, self.name))
+                pass
+
+        return missing
+
+    @property
+    def vendor(self):
+        ''' Some disks don't have ID_VENDOR_* instead the vendor
+            is encoded in the model: SanDisk_A223JJ3J3 '''
+        keys = [
+            'ID_VENDOR_FROM_DATABASE',
+            'ID_VENDOR',
+            'ID_VENDOR_ID'
+        ]
+        v = self._get_hwvalues(keys=keys, missing='Unknown Vendor')
+        if v == 'Unknown Vendor':
+            v = self.model.split('_')[0]
+        return v
+
+    @property
+    def model(self):
+        keys = [
+            'ID_MODEL_FROM_DATABASE',
+            'ID_MODEL',
+            'ID_MODEL_ID'
+        ]
+        return self._get_hwvalues(keys=keys, missing='Unknown Model')
+
+    @property
+    def serial(self):
+        keys = [
+            'ID_SERIAL',
+            'ID_SERIAL_SHORT'
+        ]
+        return self._get_hwvalues(keys=keys, missing='Unknown Serial')
+
+    @property
+    def devpath(self):
+        keys = ['DEVPATH']
+        return self._get_hwvalues(keys=keys, missing='Unknown devpath')
+
+    @property
+    def is_virtual(self):
+        return self.devpath.startswith('/devices/virtual/')
 
 
 class Storage():
-    def __init__(self):
+    def __init__(self, results={}):
+        self.results = results
         self.context = pyudev.Context()
 
+    def get_devices_by_key(self, keyname, value):
+        try:
+            storage = self.results.get('storage')
+            return [device for device in storage.keys()
+                    if storage[device][keyname] == value]
+        except (KeyError, AttributeError):
+            return []
+
+    def get_devices(self):
+        try:
+            return self.results.get('storage').keys()
+        except (KeyError, AttributeError):
+            return []
+
+    def get_partitions(self, device):
+        ''' /dev/sda '''
+        try:
+            partitions = self.get_devices_by_key('DEVTYPE', 'partition')
+            return [part for part in partitions
+                    if part.startswith(device)]
+        except (KeyError, AttributeError):
+            return []
+
+    def get_disks(self):
+        try:
+            storage = self.results.get('storage')
+            return [disk for disk in self.get_devices_by_key('MAJOR', '8')
+                    if storage[disk]['DEVTYPE'] == 'disk']
+        except (KeyError, AttributeError):
+            return []
+
     def get_device_size(self, device):
+        try:
+            hwinfo = self.results.get('storage').get(device)
+            return hwinfo.get('attrs').get('size')
+        except (KeyError, AttributeError):
+            return "0"
+
+    def _get_device_size(self, device):
         ''' device='/dev/sda' '''
         with open(os.path.join('/sys/class/block',
                   os.path.basename(device), 'size')) as d:
@@ -40,4 +153,5 @@ class Storage():
                 storage[device['DEVNAME']] = dict(device)
                 storage[device['DEVNAME']].update({'attrs': attrs})
 
+        self.results = storage
         return storage
