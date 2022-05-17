1c10b0a238f094817c19bfb97eb725a058a407cb
Dan Bungert <daniel.bungert@canonical.com>
2022-05-17 10:57:58 -0600

    filesystem: report min and cur fs sizes

--- probert/filesystem.py
+++ probert/filesystem.py
@@ -14,6 +14,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+import re
+import subprocess
 
 import pyudev
 
@@ -22,10 +24,113 @@ from probert.utils import sane_block_devices
 log = logging.getLogger('probert.filesystems')
 
 
-def get_device_filesystem(device):
+def run(cmdarr, **kw):
+    try:
+        return subprocess.check_output(cmdarr, universal_newlines=True, **kw)
+    except subprocess.CalledProcessError as cpe:
+        if cpe.stderr:
+            log.debug('stderr: %s', cpe.stderr)
+        log.exception(cpe)
+        return None
+
+
+def get_dumpe2fs_info(path):
+    ret = {}
+    out = run(['dumpe2fs', '-h', path])
+    if out is None:
+        log.debug('ext volume size not found: dumpe2fs failure')
+        return None
+    # Block count:              20480
+    # Block size:               4096
+    block_count_matcher = re.compile(r'Block count:\s+(\d+)')
+    block_size_matcher = re.compile(r'Block size:\s+(\d+)')
+    for line in out.splitlines():
+        m = block_count_matcher.match(line)
+        if m:
+            ret['block_count'] = int(m.group(1))
+        m = block_size_matcher.match(line)
+        if m:
+            ret['block_size'] = int(m.group(1))
+    if 'block_count' not in ret or 'block_size' not in ret:
+        log.debug('ext volume size not found: unexpected output format')
+        return None
+    return ret
+
+
+def get_resize2fs_info(path):
+    # Estimated minimum size of the filesystem: 1696
+    out = run(['resize2fs', '-P', path])
+    if out is None:
+        return None
+    min_blocks_matcher = re.compile(
+        r'^Estimated minimum size of the filesystem: (\d+)')
+    for line in out.splitlines():
+        m = min_blocks_matcher.match(line)
+        if m:
+            return {'min_blocks': int(m.group(1))}
+    return None
+
+
+def get_ext_sizing(path):
+    dumpe2fs_info = get_dumpe2fs_info(path)
+    if not dumpe2fs_info:
+        return None
+    ret = {'SIZE': dumpe2fs_info['block_count'] * dumpe2fs_info['block_size']}
+
+    resize2fs_info = get_resize2fs_info(path)
+    if resize2fs_info:
+        min_size = resize2fs_info['min_blocks'] * dumpe2fs_info['block_size']
+        ret['ESTIMATED_MIN_SIZE'] = min_size
+    return ret
+
+
+def get_ntfs_sizing(path):
+    cmd = ['ntfsresize',
+           '--no-action',
+           '--force',  # needed post-resize, which otherwise demands a CHKDSK
+           '--info', path]
+    out = run(cmd)
+    if out is None:
+        log.debug('ntfs volume size not found: ntfsresize failure')
+        return None
+    # Sample input:
+    #   Current volume size: 41939456 bytes (42 MB)
+    #   ...
+    #   You might resize at 2613248 bytes or 3 MB (freeing 39 MB).
+    volsize_matcher = re.compile(r'^Current volume size: ([0-9]+) bytes')
+    minsize_matcher = re.compile(r'^You might resize at ([0-9]+) bytes')
+    ret = {}
+    for line in out.splitlines():
+        m = volsize_matcher.match(line)
+        if m:
+            ret['SIZE'] = int(m.group(1))
+        m = minsize_matcher.match(line)
+        if m:
+            ret['ESTIMATED_MIN_SIZE'] = int(m.group(1))
+    if 'SIZE' not in ret:
+        log.debug('ntfs volume size not found: unexpected output format')
+    return ret
+
+
+sizing_tools = {
+    'ext2': get_ext_sizing,
+    'ext3': get_ext_sizing,
+    'ext4': get_ext_sizing,
+    'ntfs': get_ntfs_sizing,
+}
+
+
+def get_device_filesystem(device, sizing):
     # extract ID_FS_* keys into dict, dropping leading ID_FS
-    return {k.replace('ID_FS_', ''): v
-            for k, v in device.items() if k.startswith('ID_FS_')}
+    fs_info = {k.replace('ID_FS_', ''): v
+               for k, v in device.items() if k.startswith('ID_FS_')}
+    if sizing:
+        fstype = fs_info.get('TYPE', None)
+        if fstype in sizing_tools:
+            size_info = sizing_tools[fstype](device.device_node)
+            if size_info is not None:
+                fs_info.update(size_info)
+    return fs_info
 
 
 def probe(context=None, enabled_probes=None, **kw):
@@ -34,11 +139,13 @@ def probe(context=None, enabled_probes=None, **kw):
     if not context:
         context = pyudev.Context()
 
+    need_fs_sizing = 'filesystem_sizing' in enabled_probes
+
     for device in sane_block_devices(context):
         # Ignore block major=1 (ramdisk) and major=7 (loopback)
         # these won't ever be used in recreating storage on target systems.
         if device['MAJOR'] not in ["1", "7"]:
-            fs_info = get_device_filesystem(device)
+            fs_info = get_device_filesystem(device, need_fs_sizing)
             # The ID_FS_ udev values come from libblkid, which contains code to
             # recognize lots of different things that block devices or their
             # partitions can contain (filesystems, lvm PVs, bcache, ...).  We
---
+++ probert/tests/test_filesystem.py
@@ -0,0 +1,83 @@
+# Copyright 2021 Canonical, Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import random
+import string
+
+from unittest import TestCase
+from unittest.mock import patch
+
+from probert.filesystem import (
+    get_dumpe2fs_info,
+    get_resize2fs_info,
+    get_ext_sizing,
+    get_ntfs_sizing,
+)
+
+
+def read_file(filename):
+    with open(filename, 'r') as fp:
+        return fp.read()
+
+
+def random_string(length=8):
+    return ''.join(
+        random.choice(string.ascii_lowercase) for _ in range(length))
+
+
+class TestFilesystem(TestCase):
+    @patch('probert.filesystem.run')
+    def test_dumpe2fs_simple_output(self, run):
+        run.return_value = '''
+Block count: 1234
+Block size:  4000
+'''
+        expected = {'block_count': 1234, 'block_size': 4000}
+        self.assertEqual(expected, get_dumpe2fs_info(random_string()))
+
+    @patch('probert.filesystem.run')
+    def test_dumpe2fs_real_output(self, run):
+        run.return_value = read_file('probert/tests/data/dumpe2fs_ext4.out')
+        expected = {'block_count': 10240, 'block_size': 4096}
+        self.assertEqual(expected, get_dumpe2fs_info(random_string()))
+
+    @patch('probert.filesystem.run')
+    def test_resize2fs(self, run):
+        run.return_value = 'Estimated minimum size of the filesystem: 1371\n'
+        expected = {'min_blocks': 1371}
+        self.assertEqual(expected, get_resize2fs_info(random_string()))
+
+    @patch('probert.filesystem.get_resize2fs_info')
+    @patch('probert.filesystem.get_dumpe2fs_info')
+    def test_ext4(self, dumpe2fs, resize2fs):
+        dumpe2fs.return_value = {'block_count': 20000, 'block_size': 1000}
+        resize2fs.return_value = {'min_blocks': 4000}
+        expected = {'SIZE': 20000 * 1000, 'ESTIMATED_MIN_SIZE': 4000 * 1000}
+        self.assertEqual(expected, get_ext_sizing(random_string()))
+
+    @patch('probert.filesystem.run')
+    def test_ntfs_real_output(self, run):
+        run.return_value = read_file('probert/tests/data/ntfsresize.out')
+        expected = {'SIZE': 41939456, 'ESTIMATED_MIN_SIZE': 2613248}
+        self.assertEqual(expected, get_ntfs_sizing(random_string()))
+
+    @patch('probert.filesystem.run')
+    def test_ntfs_simple_output(self, run):
+        run.return_value = '''
+Current volume size: 100000000 bytes (100 MB)
+You might resize at 25000000 bytes or 25 MB (freeing 75 MB).
+'''
+        expected = {'SIZE': 100000000, 'ESTIMATED_MIN_SIZE': 25000000}
+        self.assertEqual(expected, get_ntfs_sizing(random_string()))
