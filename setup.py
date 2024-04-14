6e52e05edc9b341da5763c28f4f98ece7a997fc5
Michael Hudson-Doyle <michael.hudson@canonical.com>
2024-04-15 09:16:20 +1200

    do not pass strict versions to install_requires
    It does not make sense for install_requires to specify a version of
    jsonschema that is over 7 years old.

--- setup.py
+++ setup.py
@@ -41,11 +41,6 @@ def pkgconfig(package):
         'extra_link_args': subprocess.check_output(['pkg-config', '--libs', package]).decode('utf8').split(),
     }
 
-
-def read_requirement():
-    return [req.strip() for req in open('requirements.txt')]
-
-
 setup(name='probert',
       version=probert.__version__,
       description="Hardware probing tool",
@@ -66,6 +61,6 @@ setup(name='probert',
             **pkgconfig("libnl-genl-3.0")),
           ],
       packages=find_packages(),
-      install_requires=read_requirement(),
+      install_requires=['jsonschema', 'pyudev'],
       include_package_data=True,
 )
