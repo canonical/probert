8ad4af92a36aae5599adc63b4596720ac12538ec
Olivier Gayot <olivier.gayot@canonical.com>
2025-03-14 18:27:46 +0100

    network: instantiate the WLAN listener earlier
    Signed-off-by: Olivier Gayot <olivier.gayot@canonical.com>

--- probert/network.py
+++ probert/network.py
@@ -641,7 +641,11 @@ class UdevObserver(NetworkObserver):
         assert isinstance(receiver, NetworkEventReceiver)
         self.receiver = receiver
         self._calls = None
-        self.with_wlan_listener = with_wlan_listener
+
+        if with_wlan_listener:
+            self.wlan_listener = _nl80211.listener(self)
+        else:
+            self.wlan_listener = None
 
     def start(self):
         self.rtlistener = _rtnetlink.listener(self)
@@ -652,9 +656,8 @@ class UdevObserver(NetworkObserver):
             self.rtlistener.fileno(): self.rtlistener.data_ready,
         }
 
-        if self.with_wlan_listener:
+        if self.wlan_listener is not None:
             try:
-                self.wlan_listener = _nl80211.listener(self)
                 self.wlan_listener.start()
                 self._fdmap.update({
                     self.wlan_listener.fileno(): self.wlan_listener.data_ready,
@@ -736,7 +739,7 @@ class UdevObserver(NetworkObserver):
         self.receiver.route_change(action, data)
 
     def trigger_scan(self, ifindex):
-        if not self.with_wlan_listener:
+        if self.wlan_listener is None:
             log.debug("ignoring request to trigger a scan: no WLAN listener")
             return
         self.wlan_listener.trigger_scan(ifindex)
